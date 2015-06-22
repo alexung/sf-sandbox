4fb5ac9234b6ea0818508436edf97ec3c6c669c9
Alex Ung <aung@aung-ltm.internal.salesforce.com>
2015-06-22 16:41:37 -0700

    Made page more modular and fit all components on the helpTrainingPage

--- app/app.routes.js
+++ app/app.routes.js
@@ -11,48 +11,85 @@
 sfApp.config(function($stateProvider, $urlRouterProvider) {
 
 	$stateProvider
+		// .state("root", {
+		// 	url: '/',
+		// 	templateUrl: "app/components/help-training/views/helpTrainingView.html",
+		// 	views: {
+		// 		"header": { 
+		// 			templateUrl: "app/components/global/partials/globalHeader.html",
+		// 			controller: "helpTrainingCtrl" 
+		// 		},
+		// 		"searchBar@root": {
+		// 			templateUrl: "app/components/help-training/views/partials/searchBar.html",
+		// 			controller: "helpTrainingCtrl"
+		// 		},
+		// 		"sixBoxes": {
+		// 			templateUrl: "app/components/help-training/views/partials/sixBoxes.html",
+		// 			controller: "helpTrainingCtrl"
+		// 		},
+		// 		"topSolutions": {
+		// 			templateUrl: "app/components/help-training/views/partials/topSolutions.html",
+		// 			controller: "helpTrainingCtrl"
+		// 		},
+		// 		"additionalHelpTraining": {
+		// 			templateUrl: "app/components/help-training/views/partials/additionalHelpTraining.html",
+		// 			controller: "helpTrainingCtrl"
+		// 		},
+		// 		"upgradeStatement": {
+		// 			templateUrl: "app/components/help-training/views/partials/upgradeStatement.html",
+		// 			controller: "helpTrainingCtrl"
+		// 		},
+		// 		"footer": {
+		// 			templateUrl: "app/components/global/partials/globalFooter.html",
+		// 			controller: "helpTrainingCtrl"
+		// 		}
+		// 	}
+
+
+		//})
+
 		.state("root", {
-			url: '/',
-			views: {
-				"header": { 
-					templateUrl: "app/components/global/partials/globalHeader.html",
-					controller: "helpTrainingCtrl" 
-				},
-				"searchBar": {
-					templateUrl: "app/components/help-training/views/partials/searchBar.html",
-					controller: "helpTrainingCtrl"
-				},
-				"sixBoxes": {
-					templateUrl: "app/components/help-training/views/partials/sixBoxes.html",
-					controller: "helpTrainingCtrl"
-				},
-				"topSolutions": {
-					templateUrl: "app/components/help-training/views/partials/topSolutions.html",
-					controller: "helpTrainingCtrl"
-				},
-				"additionalHelpTraining": {
-					templateUrl: "app/components/help-training/views/partials/additionalHelpTraining.html",
-					controller: "helpTrainingCtrl"
-				},
-				"upgradeStatement": {
-					templateUrl: "app/components/help-training/views/partials/upgradeStatement.html",
-					controller: "helpTrainingCtrl"
-				},
-				"footer": {
-					templateUrl: "app/components/global/partials/globalFooter.html",
-					controller: "helpTrainingCtrl"
-				},
-				"restOfTemplate": { 
-					templateUrl: "app/components/help-training/views/helpTrainingView.html",
-					controller: "helpTrainingCtrl"
+				url: "/",
+				views: {
+					"helpTrainingView":	{
+						templateUrl: "app/components/help-training/views/helpTrainingView.html",
+						controller: "helpTrainingCtrl"
+					},
+					"header@root": { 
+						templateUrl: "app/components/global/partials/globalHeader.html",
+						controller: "helpTrainingCtrl" 
+					},
+					"searchBar@root": {
+						templateUrl: "app/components/help-training/views/partials/searchBar.html",
+						controller: "helpTrainingCtrl"
+					},
+					"sixBoxes@root": {
+						templateUrl: "app/components/help-training/views/partials/sixBoxes.html",
+						controller: "helpTrainingCtrl"
+					},
+					"topSolutions@root": {
+						templateUrl: "app/components/help-training/views/partials/topSolutions.html",
+						controller: "helpTrainingCtrl"
+					},
+					"additionalHelpTraining@root": {
+						templateUrl: "app/components/help-training/views/partials/additionalHelpTraining.html",
+						controller: "helpTrainingCtrl"
+					},
+					"upgradeStatement@root": {
+						templateUrl: "app/components/help-training/views/partials/upgradeStatement.html",
+						controller: "helpTrainingCtrl"
+					},
+					"footer@root": {
+						templateUrl: "app/components/global/partials/globalFooter.html",
+						controller: "helpTrainingCtrl"
+					}
+
 				}
 
-			}
-		})
-
+			})
 
 		$urlRouterProvider
-		.otherwise('/');
+			.otherwise('/');
 		
 
 });
