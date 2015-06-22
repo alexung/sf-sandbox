3347895a8382b184639bf2b0f592608ab9ddd640
Alex Ung <aung@aung-ltm.internal.salesforce.com>
2015-06-22 15:43:59 -0700

    Made app more modular by separating the helpTrainingView into multiple components

--- app/app.routes.js
+++ app/app.routes.js
@@ -22,6 +22,26 @@ sfApp.config(function($stateProvider, $urlRouterProvider) {
 					templateUrl: "app/components/help-training/views/partials/searchBar.html",
 					controller: "helpTrainingCtrl"
 				},
+				"sixBoxes": {
+					templateUrl: "app/components/help-training/views/partials/sixBoxes.html",
+					controller: "helpTrainingCtrl"
+				},
+				"topSolutions": {
+					templateUrl: "app/components/help-training/views/partials/topSolutions.html",
+					controller: "helpTrainingCtrl"
+				},
+				"additionalHelpTraining": {
+					templateUrl: "app/components/help-training/views/partials/additionalHelpTraining.html",
+					controller: "helpTrainingCtrl"
+				},
+				"upgradeStatement": {
+					templateUrl: "app/components/help-training/views/partials/upgradeStatement.html",
+					controller: "helpTrainingCtrl"
+				},
+				"footer": {
+					templateUrl: "app/components/global/partials/globalFooter.html",
+					controller: "helpTrainingCtrl"
+				},
 				"restOfTemplate": { 
 					templateUrl: "app/components/help-training/views/helpTrainingView.html",
 					controller: "helpTrainingCtrl"
